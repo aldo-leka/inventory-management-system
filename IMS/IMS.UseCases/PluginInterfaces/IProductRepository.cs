a59c150802a3223015bbf229474f90afc4e17a6a
Aldo <aldo.leka@live.com>
2025-03-19 17:12:58 +0100

    Lesson 40 - Add products repository

---
+++ IMS/IMS.UseCases/PluginInterfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using IMS.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    public interface IProductRepository
+    {
+        Task AddProductAsync(Product inventory);
+        Task DeleteProductByIdAsync(int inventoryId);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<Product> GetProductByIdAsync(int inventoryId);
+        Task UpdateProductAsync(Product inventory);
+    }
+}
