53131a4eb5d154173ad558955c223f39d8f78654
Aldo <aldo.leka@live.com>
2025-03-19 12:06:26 +0100

    Lesson 24 - Extract InventoryListItemComponent & add add-inventory use case

--- IMS/IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS/IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -11,11 +11,26 @@ namespace IMS.Plugins.InMemory
         {
             _inventories = new List<Inventory>()
             {
-                new Inventory {InventoryId = 1, InventoryName="Bike Seat", Quantity=10, Price=2},
-                new Inventory {InventoryId = 2, InventoryName="Bike Body", Quantity=10, Price=15},
-                new Inventory {InventoryId = 3, InventoryName="Bike Wheels", Quantity=20, Price=8},
-                new Inventory {InventoryId = 4, InventoryName="Bike Pedels", Quantity=20, Price=2}
+                new Inventory {InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2},
+                new Inventory {InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15},
+                new Inventory {InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8},
+                new Inventory {InventoryId = 4, InventoryName = "Bike Pedels", Quantity = 20, Price = 2}
             };
+        }
+
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+            if (_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+            {
+                return Task.CompletedTask;
+            }
+
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId + 1;
+
+            _inventories.Add(inventory);
+
+            return Task.CompletedTask;
         }
 
         public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
