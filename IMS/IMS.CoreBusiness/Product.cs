d3ec9d7d1179ad186410b8433a3f46f518aeb423
Aldo <aldo.leka@live.com>
2025-03-20 09:33:38 +0100

    Lesson 47 - Add product inventories

--- IMS/IMS.CoreBusiness/Product.cs
+++ IMS/IMS.CoreBusiness/Product.cs
@@ -15,5 +15,28 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0.")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(x => x.Inventory is not null &&
+                x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                ProductInventories.Add(new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = ProductId,
+                    Product = this
+                });
+            }
+        }
+
+        public void RemoveInventory(ProductInventory productInventory)
+        {
+            this.ProductInventories?.Remove(productInventory);
+        }
     }
 }
