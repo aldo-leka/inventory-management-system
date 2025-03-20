1e4c713a09e9f64480babfdd75e06dd23f8f649f
Aldo <aldo.leka@live.com>
2025-03-20 14:46:02 +0100

    Lesson 62 - prevent form submit on key enter

---
+++ IMS/IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,9 @@
+﻿function preventFormSubmission(formId) {
+    document.getElementById(`${formId}`).addEventListener('keydown', function (event) {
+        if (event.key === "Enter") {
+            console.log("why not working?");
+            event.preventDefault();
+            return false;
+        }
+    });
+}
