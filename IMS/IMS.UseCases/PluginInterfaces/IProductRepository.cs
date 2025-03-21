a6db7aad60c6efb3486ddef57bdf8db5cfdc6cff
Aldo <aldo.leka@live.com>
2025-03-21 10:47:31 +0100

    Lesson 77 - Add product ef core repository

--- IMS/IMS.UseCases/PluginInterfaces/IProductRepository.cs
+++ IMS/IMS.UseCases/PluginInterfaces/IProductRepository.cs
@@ -9,10 +9,10 @@ namespace IMS.UseCases.PluginInterfaces
 {
     public interface IProductRepository
     {
-        Task AddProductAsync(Product inventory);
+        Task AddProductAsync(Product product);
         Task DeleteProductByIdAsync(int inventoryId);
         Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
-        Task<Product?> GetProductByIdAsync(int inventoryId);
-        Task UpdateProductAsync(Product inventory);
+        Task<Product?> GetProductByIdAsync(int productId);
+        Task UpdateProductAsync(Product product);
     }
 }
